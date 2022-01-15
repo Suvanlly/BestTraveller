2275de7107b131ee4d73a4a4b0df2ad228f5b22a
suvanlly <swl381029812@gmail.com>
2022-01-15 21:08:03 +1100

    edit code

--- frontend-Traveller/src/pages/HomePage/Top/styles/StateTab 2.js
+++
@@ -1,9 +0,0 @@
-import { TabPanel } from "react-tabs"
-import styled from "styled-components"
-
-export const Panel = styled(TabPanel)`
-  display: flex;
-  flex-direction: row;
-  flex-wrap: wrap;
-  justify-content: center;
-`
